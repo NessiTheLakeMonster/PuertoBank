7dfff0e09c911d7b18b557de7eff56441a768a2b
Jaime Rafael Fraile Bautista <jrafaelfraile1802@gmail.com>
2023-10-03 14:20:11 +0200

    Añadimos la estructura del proyecto dado por el profeso

---
+++ PuertoBank/app.js
@@ -0,0 +1,15 @@
+
+
+function cargarDatos(){
+    /*document.getElementById('idPersona').value = persona.id
+    document.getElementById('nombre').value = persona.nombre
+    document.getElementById('apellido1').value = persona.apellido1
+    document.getElementById('apellido2').value = persona.apellido2
+    document.getElementById('nacionalidad').value = persona.nacionalidad*/
+    menu = document.getElementById('menu').innerHTML
+}
+
+function cargarCabecera(dest){  
+ document.getElementById(dest).innerHTML = '   <h1>BancoPuertollano</h1>    <ul>        <li><a href="index.html">Inicio</a></li>        <li><a href="infoCuenta.html">Informaci&#243;n Cuenta</a></li>             <li><a href="tarjetas.html">Tarjetas</a></li>    </ul>' 
+}
+
